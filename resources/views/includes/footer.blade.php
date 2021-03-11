f08c23fe22fa069b1584a2adcd3de405f631a33e
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2021-03-11 17:17:03 +0700

    Admin template view layouts

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,9 @@
+<!-- Footer -->
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>2020 &copy; NOMADS - Naufal Fadhil</span>
+        </div>
+    </div>
+</footer>
+<!-- End of Footer -->
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('admin')
+    ->namespace('Admin')
+    ->group(function () {
+        Route::get('/', 'DashboardController@index')
+            ->name('dashboard');
+    });
