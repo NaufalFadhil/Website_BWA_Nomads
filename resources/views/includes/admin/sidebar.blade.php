e6048def3c9545db83958f8fd206e0751fa14f67
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2021-03-20 12:50:52 +0700

    create method index travel pacakage in admin

--- resources/views/includes/admin/sidebar.blade.php
+++ resources/views/includes/admin/sidebar.blade.php
@@ -2,7 +2,7 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ url('/admin') }}"">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center"  href="{{ route('dashboard') }}">
         <div class="sidebar-brand-text mx-3 d-sm-block">NOMADS ADMIN</div>
     </a>
 
@@ -11,12 +11,12 @@
 
     <!-- Nav Item - Dashboard -->
     <li class="nav-item">
-        <a class="nav-link" href="index.html">
+        <a class="nav-link" href="{{ route('dashboard') }}">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span></a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="index.html">
+        <a class="nav-link" href="{{ route('travel-package.index') }}">
             <i class="fas fa-fw fa-hotel"></i>
             <span>Travel Package</span></a>
     </li>
